1dd6ee399e282dafb0a5cddeab00c82ff9596395
s.mizukami <s-mizukami@tokyoweld.com>
2024-11-19 09:11:27 +0900

    C# APP HTTP通信対応

---
+++ src/app/components/SendDataToClient.tsx
@@ -0,0 +1,52 @@
+"use client";
+
+import React, { useState } from "react";
+import CustomButton from "./CustomButton";
+
+const SendDataToClient = () => {
+  const [inputValue, setInputValue] = useState("");
+  const [responseMessage, setResponseMessage] = useState("");
+
+  const sendData = async () => {
+    try {
+      const response = await fetch("/api/sendClientData", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ message: inputValue }),       // send data
+      });
+
+      const result = await response.json();
+      setResponseMessage(result.message);
+    } catch (error) {
+      console.error("Error sending data:", error);
+      setResponseMessage("Error sending data to API route");
+    }
+  };
+
+  return (
+    <div>
+      <h1>Send Data to C# Application</h1>
+
+      <div className="flex flex-row space-x-4">
+        <input
+          type="text"
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+          placeholder="Enter your message"
+          className="border p-2 rounded flex-grow h-12 text-black"
+        />
+
+        <CustomButton
+            label="Send Data"
+            colorScheme="blue"
+            onClick={sendData}
+          />
+      </div>
+      <p>Response: {responseMessage}</p>
+    </div>
+  );
+};
+
+export default SendDataToClient;
--- src/app/page1/page.tsx
+++ src/app/page1/page.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import ComponentsSample from '../components/ComponentsSample';
 import HomeIconButton from '../components/HomeIconButton';
+import SendDataToClient from '../components/SendDataToClient';
 
 const Page1 = () => {
     return (
         <>
+        
+        <div className="ml-2 mt-2 flex flex-col self-auto">
             <HomeIconButton/>
-            <ComponentsSample/>
-        </>
+            <ComponentsSample />
+            <div className="ml-12 mt-10 p-1">
+                 <SendDataToClient/>
+             </div>    
+        </div>
+         </>
     );
 };
 
